c029f5dd08b596a371f6c5bed77ac19b5cf5709e
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2012-12-13 17:17:07 +0100

    Added convenience methods for creating communication messages

--- message.go
+++ message.go
@@ -27,6 +27,62 @@ func EncodeMessage(msg proto.Message) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+func EncodeCommunication(msg proto.Message) ([]byte, error) {
+	ret := new(task.Communication)
+
+	switch x := msg.(type) {
+		case *task.Batch:
+			tp := task.Communication_CommunicationBatch
+
+			ret.Type = &tp
+			ret.Batch = x
+		case *task.Task:
+			tp := task.Communication_CommunicationTask
+
+			ret.Type = &tp
+			ret.Task = x
+		case *task.Response:
+			tp := task.Communication_CommunicationResponse
+
+			ret.Type = &tp
+			ret.Response = x
+		case *task.Token:
+			tp := task.Communication_CommunicationToken
+
+			ret.Type = &tp
+			ret.Token = x
+		case *task.Cancel:
+			tp := task.Communication_CommunicationCancel
+
+			ret.Type = &tp
+			ret.Cancel = x
+		case *task.Identify:
+			tp := task.Communication_CommunicationIdentify
+
+			ret.Type = &tp
+			ret.Identify = x
+		case *task.Ping:
+			tp := task.Communication_CommunicationPing
+
+			ret.Type = &tp
+			ret.Ping = x
+		case *task.Progress:
+			tp := task.Communication_CommunicationProgress
+
+			ret.Type = &tp
+			ret.Progress = x
+		case *task.Notification:
+			tp := task.Communication_CommunicationNotification
+
+			ret.Type = &tp
+			ret.Notification = x
+		default:
+			return nil, fmt.Errorf("Unknown communication type")
+	}
+
+	return EncodeMessage(ret)
+}
+
 func NewCommunication(tp task.Communication_Type, ft func(*task.Communication)) *task.Communication {
 	ret := new(task.Communication)
 
@@ -106,6 +162,10 @@ func ReadMessages(reader io.Reader, ret proto.Message, cb func(interface{}, erro
 		}
 
 		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+
 			cb(nil, err)
 			break
 		}
@@ -114,4 +174,63 @@ func ReadMessages(reader io.Reader, ret proto.Message, cb func(interface{}, erro
 			break
 		}
 	}
+}
+
+func ReadCommunication(reader io.Reader, ret proto.Message, cb func(interface{}, error) bool) {
+	comm := new(task.Communication)
+
+	ReadMessages(reader, comm, func(cc interface{}, err error) bool {
+		if err != nil {
+			return cb(nil, err)
+		}
+
+		if cc == nil {
+			return cb(nil, nil)
+		}
+
+		c := cc.(*task.Communication)
+
+		var tp task.Communication_Type
+		var msg interface{}
+
+		// Convert to correct type
+		switch ret.(type) {
+			case *task.Batch:
+				tp = task.Communication_CommunicationBatch
+				msg = c.Batch
+			case *task.Task:
+				tp = task.Communication_CommunicationTask
+				msg = c.Task
+			case *task.Response:
+				tp = task.Communication_CommunicationResponse
+
+				msg = c.Response
+			case *task.Token:
+				tp = task.Communication_CommunicationToken
+				msg = c.Token
+			case *task.Cancel:
+				tp = task.Communication_CommunicationCancel
+				msg = c.Cancel
+			case *task.Identify:
+				tp = task.Communication_CommunicationIdentify
+				msg = c.Identify
+			case *task.Ping:
+				tp = task.Communication_CommunicationPing
+				msg = c.Ping
+			case *task.Progress:
+				tp = task.Communication_CommunicationProgress
+				msg = c.Progress
+			case *task.Notification:
+				tp = task.Communication_CommunicationNotification
+				msg = c.Notification
+			default:
+				return cb(nil, fmt.Errorf("Unknown communication type"))
+		}
+
+		if tp != c.GetType() {
+			return cb(nil, fmt.Errorf("Communication type %v is not the desired type %v", c.GetType(), tp))
+		}
+
+		return cb(msg, nil)
+	})
 }
