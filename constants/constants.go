85f59dcf4ddfd2b6de2fa72bb9adae07f041bd97
Jesse van den Kieboom <jessevdk@gmail.com>
2013-03-28 18:26:04 +0100

    Added command port constant

--- constants/constants.go
+++ constants/constants.go
@@ -3,6 +3,6 @@ package constants
 const (
 	DiscoveryPort  uint   = 4756
 	MasterPort     uint   = 9485
-	MonitorPort    uint   = 8175
+	CommandPort    uint   = 7123
 	DiscoveryGroup string = "225.0.0.37"
 )
